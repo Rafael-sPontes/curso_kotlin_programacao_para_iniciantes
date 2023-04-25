df84cd452f87e3972205c1fafff92b41cf064f88
Rafael Pontes <rafael.spontes@hotmail.com>
2023-04-25 20:02:14 -0300

    curso_kotlin_programacao_para_iniciantes
    Curso de Kotlin (IntelliJ IDEA)

---
+++ Aula14/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    /*
+    * Somar         (+)
+    * Subtrair      (-)
+    * Multiplicar   (*)
+    * Dividir       (/)
+    * Módulo        (%)
+    * */
+
+    var n1 = 11
+    var n2 = 2
+    var resSoma = n1 + n2
+    var resSub = n1 - n2
+    var resMult = n1 * n2
+    var resDiv = n1 / n2
+    var resMod = n1 % n2
+
+    println("Soma = $resSoma")
+    println("Subtração = $resSub")
+    println("Multiplicação = $resMult")
+    println("Divisão = $resDiv")
+    println("Módulo = $resMod")
+}
